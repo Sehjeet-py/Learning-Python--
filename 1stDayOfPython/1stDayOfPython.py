976e9cbde97d2217d9bf671109830bc96e11acf1
Sehjeet-py <justfkingcode@gmail.com>
2025-12-25 10:09:41 +0530

    Basic Python

--- 1stDayOfPython/1stDayOfPython.py
+++ 1stDayOfPython/1stDayOfPython.py
@@ -13,19 +13,91 @@ print(age)
 sum = input("Enter two number to get their sum")
 print(sum)
 # Basic python 
-
-
-
-
-
-
-
-
-
-
-
-
-
+# Single Line Comment = #
+"""
+Multi Line Comment = """"""
+"""
+# Data Types
+# String
+name = "Sehjeet"
+status = "Busy,Out of reach"
+Country = "India"
+print(name)
+print(status)
+print(Country)
+# Integer
+age = 13
+priceofproduct = 10
+numbers = 1,2,3,4
+print(age)
+print(priceofproduct)
+print(numbers)
+# Boolean
+is_lockedin = True
+is_Gaming = False
+is_Busy = True
+print(is_lockedin)
+print(is_Busy)
+print(is_Gaming)
+# Floats
+gpa = 3.6
+price = 10.99
+floats = 3.1,3.5,5.5,11.4
+print(gpa)
+print(price)
+print(floats)
+# List
+names = ["Sehjeet","Posh","Mosh","Josh"]
+ages = [13,14,18,21,23,22]
+countries = ["India","Usa","China","Uk"]
+print(names)
+print(ages)
+print(countries)
+# Tuple
+names1st = ("Sehjeet","Harry","Josh","Mosh","Posh")
+ages1st = (14,13,16,18,23,22,31)
+gpas = (3.2,2.2,4.0,2.9,3.9)
+print(names1st)
+print(ages1st)
+print(gpas)
+# Sets
+ages2nd = {13,12,15,16,18,22,26}
+names2nd = {"josh","Posh","Dosh","Sehjeet"}
+country = {"India","Usa","Uk","Austraila","China"}
+print(ages2nd)
+print(names2nd)
+print(country)
+# Dictionary
+information = {
+    "Sehjeet":13,
+    "Mosh":33,
+    "Harry":23
+}
+print(information)
+print(information["Sehjeet"])
+# Complex
+complex1 = 3j + 4j
+complex2 = 2j * 7j
+complex3 = 42j / 2j
+print(complex1)
+print(complex2)
+print(complex3)
+# Basic Math Operation
+print(2+5)
+print(4-1)
+print(7*3)
+print(40/2)
+print(2**4)
+print(3%2)
+print(3//2)
+# Checking Data Types
+print(type(2j+3))
+print(type(13))
+print(type(3.6))
+print(type("Sehjeet"))
+print(type(("sehjeet","posh")))
+print(type([13,1,13,1,3,33]))
+print(type({"India","Us","Uk"}))
 
 
 
