9ff5322a97a8df481de3e9651a045b9bcead83df
Sehjeet-py <justfkingcode@gmail.com>
2025-12-26 16:14:07 +0530

    ExerciseOf1stDay

--- 1stDayOfPython/ExerciseOf1stDay.py
+++ 1stDayOfPython/ExerciseOf1stDay.py
@@ -1,50 +1,71 @@
 # Exercises - Day 1
 # Exercise Level 1
-import cmd as sh
-print(sh)
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+# 1st Exercise
+print(3+4)
+print(3-4)
+print(3*4)
+print(3%4)
+print(3/4)
+print(3**4)
+print(3//4)
+# 2nd Exercise
+Name = "Sehjeet"
+FamilyName = "Sehjeet"
+Country = "India"
+experience = "I am enjoying 30 days of python"
+# 3rd Exercise
+print(type(10))
+print(type(9.8))
+print(type(3.14))
+print(type(4-4j))
+print(type(['Ababeneh','Python','Finland']))
+print(type("Sehjeet"))
+print(type("Sehjeet"))
+print(type("India"))
+# Exercise Level 2
+# String
+name = "Sehjeet"
+country = "India"
+status = "DND"
+# Integer 
+age = 13
+gpa = 3
+inventory = 11
+# Float
+gpa1 = 3.6
+price = 44.99
+floats = 3.6,3.2,12.1
+# Complex
+complex1 = 3**4j
+complex2 = 4+5j
+complex3 = 56-4j
+# Boolean
+Is_LockedIn = True
+Is_Available = False
+Is_Childish = False
+# List
+names = ["Sehjeet","Posh","Mosh","Dosh"]
+ages = [13,1,3,1,4,14,4,3,3,7,7,4]
+gpas = [3.4,4.0,3.6]
+# Tuple
+Names1st = ("Sehjeet","Posh","Mosh","Dosh")
+Ages = (13,1,3,1,4,14,4,3,3,7,7,4)
+gpas = (3.4,4.0,3.6)
+# Set
+Names2nd = {"Sehjeet","Posh","Mosh","Dosh"}
+age1st = {13,14,13,12,34,21,42,32,24}
+gpa1st = {3.6,3.3,4.0,2.0}
+# Dictionary
+info = {
+    "Sehjeet":13,
+    "Gurkaran":22,
+    "Gursharan":22
+}
+# Euclidian Distance
+xset = [2,3]
+yset = [10,8]
+total = (yset[0]-xset[0])**2 + (yset[1]-xset[1])**2
+print(total)
 
 
 
